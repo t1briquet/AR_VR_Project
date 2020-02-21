c273df610ae5819968394c6822fc3083630d8b67
Surbayrole Alexis <frieeze@users.noreply.github.com>
2020-02-21 16:30:07 +0100

    big keys

---
+++ Assets/Scripts/Home/KeyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = default;
+    private Transform _keyTransform = default;
+    private float _rotationAngle = default;
+
+
+    private void Awake()
+    {
+        _keyTransform = transform;
+    }
+
+    private void Update()
+    {
+        _rotationAngle = (_rotationAngle + rotationSpeed) % 360;
+        _keyTransform.rotation = Quaternion.Euler(0f, _rotationAngle, 0f);
+    }
+}
