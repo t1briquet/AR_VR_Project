3f042c0d86b82d7bf26eaff5484a984ff2107ac7
Surbayrole Alexis <frieeze@users.noreply.github.com>
2020-03-03 12:02:35 +0100

    commencé la scène 2

--- Assets/Scripts/Home/GenerateKeys.cs
+++ Assets/Scripts/Home/GenerateKeys.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditorInternal;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GenerateKeys : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject key = default;
+
+    private GameObject keysGO;
+    private Transform keysTransform;
+
+    private void Awake()
     {
-        
+        keysGO = gameObject;
+        keysTransform = transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(SpawnKeys());
+    }
+
+    private IEnumerator SpawnKeys()
+    {
+        yield return new WaitForSeconds(0.01f);
+
+        if (keysTransform.childCount == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Instantiate(key, GetRandomKeyPoition(), Quaternion.identity, keysTransform);
+            }
+        }
+    }
+
+    private Vector3 GetRandomKeyPoition()
+    {
+        float x = Random.Range(transform.position.x - 95f, transform.position.x + 95f);
+        float z = Random.Range(transform.position.x - 45f, transform.position.y + 45f);
+        return new Vector3(x, transform.position.y, z);
     }
 }
